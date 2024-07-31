43a4e1e27d6ecd09226193389dbabefc2d3b6c3a
João Renato Silva <joaormmsilva@gmail.com>
2024-07-31 20:52:48 -0300

    novo inicio

--- javascript_CursoEmVideo/ex010/.js
+++
@@ -1,9 +0,0 @@
-function evenOrOdd(number) {
-    let number = 1
-    
-    if (number => 1){
-      return ('Even')
-    }else if (number <= -1){
-      return ('Odd')
-    }
-  }
---
+++ javascript_CursoEmVideo/mod2/tipos.js
@@ -0,0 +1,10 @@
+var nome = "joão"
+var numero = 24
+var vazio 
+var listaArray = [1,2,3,4,5,]
+var objeto = []
+console.log(typeof(nome))
+console.log(typeof(numero))
+console.log(typeof(vazio))
+console.log(typeof(listaArray))
+console.log(typeof(objeto))
