0201bdfb77ae68375493765b73ebe586ce08c5b2
João Renato Silva <joaormmsilva@gmail.com>
2024-08-03 11:57:13 -0300

    while e for

---
+++ javascript_CursoEmVideo/mod5/for/teste.js
@@ -0,0 +1,15 @@
+var fatias = [1,2,3,4,5,6,7,8] 
+
+for(comerPizza = 1;  comerPizza <= fatias.length; comerPizza++){
+    console.log(`Você comeu ${comerPizza} fatia`)
+}
+
+var agachamentos = 11
+
+for(termino = 1; termino <= agachamentos; termino++){
+    if(termino == 11){
+        console.log(`voce termino parabens`)
+    }else{
+        console.log(`Você fez ${termino} agachamentos, ainda falta mais alguns`)
+    }
+}
---
+++ javascript_CursoEmVideo/mod5/while_doWhile/teste.js
@@ -0,0 +1,16 @@
+var pizza = 1
+
+//-------------------------------------
+
+do{
+    console.log(`comeu ${pizza}`)
+    pizza++
+}while(pizza <= 8)
+
+//-------------------------------------
+
+while(pizza <= 8){
+    console.log(`comeu ${pizza}`)
+    pizza++
+}
+
