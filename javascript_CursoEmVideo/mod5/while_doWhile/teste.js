7047a1c32bb05ff7f9352112f4cc564fae2b267e
João Renato Silva <joaormmsilva@gmail.com>
2024-08-05 16:24:01 -0300

    atividade

--- javascript_CursoEmVideo/mod5/while_doWhile/teste.js
+++ javascript_CursoEmVideo/mod5/while_doWhile/teste.js
@@ -1,16 +1,15 @@
 var pizza = 1
 
-//-------------------------------------
+//------------enquanto--------------//
 
 do{
     console.log(`comeu ${pizza}`)
     pizza++
 }while(pizza <= 8)
 
-//-------------------------------------
+//------------enquanto--------------//
 
 while(pizza <= 8){
     console.log(`comeu ${pizza}`)
     pizza++
 }
-
