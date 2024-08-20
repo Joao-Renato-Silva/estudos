12d77d7b68c0305354ef59e2af736ba7133e7591
João Renato Silva <joaormmsilva@gmail.com>
2024-08-20 15:13:37 -0300

    testes

--- javascript_CursoEmVideo/mod2/operadores/logicos.js
+++ javascript_CursoEmVideo/mod2/operadores/logicos.js
@@ -6,3 +6,5 @@ console.log((x == 2) && (y == 3))//os dois verdadeiros
 console.log((x > 1)  || (y < 3))//1 pelo menos verdadeiro
 
 console.log(!(x < y))//inverte o resultado
+
+console.log(`o seu x é igual a ${x}` ?? `valor não informado`)// ou escreve um ou outro
